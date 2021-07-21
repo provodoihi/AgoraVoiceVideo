9386725c984a900a766d013e5a7d8962e2502b24
provodoihi <profiloes@gmail.com>
2021-07-21 11:21:10 +0700

    Add firebase notification

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import {
   View,
   TouchableOpacity,
   ImageBackground,
   Text,
   Image,
-  StyleSheet,
+  Alert,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import messaging from '@react-native-firebase/messaging';
 import { AppNavigationProps } from './navigation/routes';
+import styles from './stylehome';
 
 const Home = ({ navigation }: AppNavigationProps<'Home'>) => {
+  const requestUserPermission = useCallback(async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      getFcmToken();
+      console.log('Authorization status:', authStatus);
+    }
+  }, []);
+
+  const getFcmToken = async () => {
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+      console.log(fcmToken);
+      console.log('Your Firebase Token is:', fcmToken);
+    } else {
+      console.log('Failed', 'No token received');
+    }
+  };
+
+  useEffect(() => {
+    requestUserPermission();
+    const unsubscribe = messaging().onMessage(() => {
+      Alert.alert('React Native Call', 'You got a call');
+    });
+    return unsubscribe;
+  }, [requestUserPermission]);
+
   return (
     <ImageBackground
       blurRadius={3}
@@ -42,95 +74,4 @@ const Home = ({ navigation }: AppNavigationProps<'Home'>) => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    textAlign: 'center',
-    marginTop: '2%',
-    borderColor: '#fff',
-    borderWidth: 1,
-    width: '95%',
-    borderRadius: 20,
-    paddingHorizontal: 10,
-    paddingVertical: 15,
-    color: '#fff',
-  },
-  box1: {
-    flex: 1.2,
-    alignItems: 'center',
-  },
-  box2: {
-    marginTop: '2%',
-    flex: 0.5,
-    alignItems: 'center',
-  },
-  callBox: {
-    flex: 1,
-    alignItems: 'center',
-    width: '100%',
-    marginBottom: '5%',
-  },
-  text: {
-    marginTop: '5%',
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: '500',
-  },
-  call: {
-    width: 144,
-    height: 144,
-    resizeMode: 'contain',
-  },
-  settingBox: {
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    width: '100%',
-    flexDirection: 'row',
-  },
-  settingBox2: {
-    marginTop: '1.5%',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    width: '100%',
-    flexDirection: 'row',
-  },
-  button: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-    borderWidth: 1,
-    borderColor: '#fff',
-    width: 72,
-    height: 72,
-    borderRadius: 36,
-  },
-  usersListContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  floatRight: {
-    height: 72,
-    width: 72,
-    borderRadius: 36,
-    borderWidth: 1,
-    borderColor: '#fff',
-    backgroundColor: 'transparent',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  txt: {
-    fontSize: 16,
-    color: '#fff',
-  },
-  txtBig: {
-    fontSize: 24,
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-});
 export default Home;
---
+++ src/stylehome.ts
@@ -0,0 +1,95 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    textAlign: 'center',
+    marginTop: '2%',
+    borderColor: '#fff',
+    borderWidth: 1,
+    width: '95%',
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    color: '#fff',
+  },
+  box1: {
+    flex: 1.2,
+    alignItems: 'center',
+  },
+  box2: {
+    marginTop: '2%',
+    flex: 0.5,
+    alignItems: 'center',
+  },
+  callBox: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: '5%',
+  },
+  text: {
+    marginTop: '5%',
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  call: {
+    width: 144,
+    height: 144,
+    resizeMode: 'contain',
+  },
+  settingBox: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '100%',
+    flexDirection: 'row',
+  },
+  settingBox2: {
+    marginTop: '1.5%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '100%',
+    flexDirection: 'row',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: '#fff',
+    width: 72,
+    height: 72,
+    borderRadius: 36,
+  },
+  usersListContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  floatRight: {
+    height: 72,
+    width: 72,
+    borderRadius: 36,
+    borderWidth: 1,
+    borderColor: '#fff',
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  txt: {
+    fontSize: 16,
+    color: '#fff',
+  },
+  txtBig: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
